9a9aa6a4c6e5cad1e88fcfad2171b2306a8808cc
Macarena Benitez <bmacarenasolange@gmail.com>
2026-04-22 15:07:22 -0300

    session quienes somos

--- resources/views/quienes_somos.blade.php
+++ resources/views/quienes_somos.blade.php
@@ -1,18 +1,25 @@
 <section class="sobre-nosotros">
-
+<head>    
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}"></head> 
     <div class="container-fluid p-0">
         <div class="row g-0">
 
             <!-- LADO IZQUIERDO -->
             <div class="col-md-6 lado-izquierdo d-flex align-items-center justify-content-center">
-                <h1 class="titulo-izquierdo">¿Quiénes somos?</h1>
-            </div>
+                 <div class="hero-info text-center">
+        <h1>Transformá tu cuerpo con <span class="marca">EVOLVEX</span></h1>
+        
+        <p>
+            Suplementos y accesorios diseñados para llevar tu rendimiento al siguiente nivel.
+        </p>
 
+        <a href="#" class="btn-principal">Ver productos</a>
+    </div>
             <!-- LADO DERECHO -->
             <div class="col-md-6 lado-derecho d-flex align-items-center">
                 <div class="contenido-texto text-center">
-
-                    <h2 class="titulo-derecho">Suplementos y accesorios EVOLVEX</h2>
+                    
+                <h2 class="titulo-derecho">¿Quiénes somos?</h2>
 
                     <p>
                         Más que una tienda, somos el aliado que necesitás para transformar tu cuerpo.
@@ -24,6 +31,13 @@
                         Somos una joven compañía enfocada en la venta y distribución de suplementación y accesorios deportivos.
                         Nuestro objetivo es llegar a cada rincón del país, brindando un servicio rápido, confiable y accesible.
                     </p>
+                    <h3 class="origen-marca">¿Por qué EVOLVEX?</h3>
+                 <p>
+                    EVOLVE representa el cambio, el progreso constante y la disciplina de superarse cada día.
+                 La “X” simboliza el punto donde rompés tus propios límites.
+
+                 EVOLVEX no es solo un nombre, es una mentalidad: evolucionar más allá de lo esperado.
+                </p>
 
                 </div>
             </div>
